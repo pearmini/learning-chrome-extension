cde2affbfbdd9383cd075df4355b69870122d611
Bairui Su <subairui@icloud.com>
2024-11-22 12:44:44 -0500

    Add Reading Time (#1)

---
+++ reading_time/scripts/content.js
@@ -0,0 +1,23 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `👋 Hi Jim! ⏱️ ${readingTime} min read`;
+  badge.style = "background-color: #f0f0f0; padding: 0.5rem;border-radius: 0.25rem; width: fit-content;";
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
